aa92a42aa0a7a69ed597fb41d1507e00b8a22d39
Martin Mareš <mares@datalite.cz>
2022-01-22 16:00:56 +0100

    Refactor config

--- src/config.cr
+++ src/config.cr
@@ -1,21 +1,81 @@
 require "yaml"
-require "../src/checks"
 
 module TsmHealthCheck
   class Config
-    @config_yaml : ConfigYaml
+    include YAML::Serializable
 
-    def initialize(@config_yaml)
-    end
+    @[YAML::Field(key: "checks")]
+    property checks : Array(Check)
 
     def self.load(from = file)
       content = File.read(from)
-      config_yaml = ConfigYaml.from_yaml(content)
-      self.new(config_yaml)
+      self.from_yaml(content)
     end
 
     def each_with_checks
-      @config_yaml.checks.each { |check| yield(check) }
+      checks.each { |check| yield(check) }
     end
+  end
+
+  class Check
+    include YAML::Serializable
+
+    @[YAML::Field(key: "name")]
+    property name : String
+
+    @[YAML::Field(key: "endpoint")]
+    property endpoint : String
+
+    @[YAML::Field(key: "up")]
+    property up : Up
+
+    @[YAML::Field(key: "down")]
+    property down : Down
+  end
+
+  class Up
+    include YAML::Serializable
+
+    @[YAML::Field(key: "timeout")]
+    property timeout : Int32
+
+    @[YAML::Field(key: "status_code")]
+    property status_code : Int32
+
+    @[YAML::Field(key: "json")]
+    property json : Json
+
+    @[YAML::Field(key: "response")]
+    property response : Response
+  end
+
+  class Json
+    include YAML::Serializable
+
+    @[YAML::Field(key: "path")]
+    property path : String
+
+    @[YAML::Field(key: "value")]
+    property value : String
+  end
+
+  class Response
+    include YAML::Serializable
+
+    @[YAML::Field(key: "status_code")]
+    property status_code : String
+
+    @[YAML::Field(key: "content_type")]
+    property content_type : String
+
+    @[YAML::Field(key: "body")]
+    property body : String
+  end
+
+  class Down
+    include YAML::Serializable
+
+    @[YAML::Field(key: "response")]
+    property response : Response
   end
 end
